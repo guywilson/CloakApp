2dabc7913921163fce1e9378627fb60b52837e2e
Guy Wilson <wilson.guy@gmail.com>
2021-02-27 21:00:13 +0000

    Initial Commit

---
+++ Cloak/CloakApp.swift
@@ -0,0 +1,17 @@
+//
+//  CloakApp.swift
+//  Cloak
+//
+//  Created by Guy Wilson on 27/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct CloakApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
